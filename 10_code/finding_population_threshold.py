a9287e6b09079b3b1dcb6b7ef326e5549c342bca
Aarushi Verma <Aarushi@Aarushis-MBP.lan>
2021-11-19 19:28:16 -0500

    updated mortality file with population threshold

--- 10_code/finding_population_threshold.py
+++ 10_code/finding_population_threshold.py
@@ -7,8 +7,10 @@ import numpy as np
 
 
 # %%
-popinc = pd.read_csv("/mnt/c/Users/sdona/Documents/Duke/720IDS/Mid-SemesterProject/pds2021-opioids-team-2-ids720/20_intermediate_files/population_2000_2020_inc.csv")
-death = pd.read_csv("/mnt/c/Users/sdona/Documents/Duke/720IDS/Mid-SemesterProject/pds2021-opioids-team-2-ids720/20_intermediate_files/vital_stata.csv")
+popinc = pd.read_csv(
+    "https://media.githubusercontent.com/media/MIDS-at-Duke/pds2021-opioids-team-2-ids720/data_merging/20_intermediate_files/population_2000_2020_inc.csv?token=AVKGWH7WSP7EA3VGYGCO7NDBTBDSQ"
+)
+death = pd.read_csv("../20_intermediate_files/vital_stata.csv")
 
 
 # %%
@@ -16,24 +18,24 @@ death = pd.read_csv("/mnt/c/Users/sdona/Documents/Duke/720IDS/Mid-SemesterProjec
 popinc.head()
 
 # %%
-#Population Income data types
+# Population Income data types
 popinc.info()
 
 # %%
 # %%
-#Vital Statistics data types
+# Vital Statistics data types
 death.info()
 
 
 # %%
 # Dropping the unnamed column
-death.drop("Unnamed: 0", axis = 1, inplace=True)
+death.drop("Unnamed: 0", axis=1, inplace=True)
 
 # %%
 
 # %%
 # Changing columns name for merging
-popinc = popinc.rename({"CTYNAME":"County_Name"}, axis =1)
+popinc = popinc.rename({"CTYNAME": "County_Name"}, axis=1)
 popinc
 
 
@@ -73,35 +75,81 @@ merged = pd.merge(
 merged._merge.value_counts()
 
 # %%
-#Viewing the merged dataset
+# Viewing the merged dataset
 merged.head()
 
 # %%
 # %%
 # Verifying whether the data merged correctly
 dropped_left = merged[merged["_merge"] != "left_only"]
-dropped_left['_merge'].value_counts()
+dropped_left["_merge"].value_counts()
 # There are cases where data was only available in left column
 
+# Subsetting the data for the years in our analysis
 # %%
 years = range(2003, 2016)
 dropped_left = dropped_left.loc[dropped_left["Year"].isin(years)]
 
+# Adding column to assess where mortality data is available
 # %%
 dropped_left["bad_merge"] = np.where(dropped_left["_merge"] == "both", 0, 1)
-dropped_left['bad_merge'].value_counts()
+dropped_left["bad_merge"].value_counts()
 dropped_left
 
+# Grouping by county to assess share  of mortality data against popn
 # %%
-merge_grouped = dropped_left.groupby(["State_Code","County_Name"],as_index=False)[['Population','bad_merge']].mean()
+merge_grouped = dropped_left.groupby(["State_Code", "County_Name"], as_index=False)[
+    ["Population", "bad_merge"]
+].mean()
 merge_grouped["bad_merge"].unique()
 
 # %%
 merge_grouped
 
+
 # %%
 import altair as alt
 
-alt.Chart(merge_grouped[merge_grouped.Population < 1_000_000]).encode(x = "Population",y=alt.Y("bad_merge", title = "Missing Mortality Share", scale=alt.Scale(zero=False))).mark_point()
+alt.Chart(merge_grouped[merge_grouped.Population < 1_000_000]).encode(
+    x="Population",
+    y=alt.Y("bad_merge", title="Missing Mortality Share", scale=alt.Scale(zero=False)),
+).mark_point()
+
+# Based on the plotting taking a cutoff based on population for counties
+# %%
+subset = merge_grouped.loc[merge_grouped["Population"] >= 500000].copy()
+subset["indicator"] = subset["State_Code"] +"-"+subset["County_Name"]
+county=list(subset.indicator.unique())
+
+# %%
+vs_pop_thresh = dropped_left.copy()
+vs_pop_thresh.head()
+
+# %%
+vs_pop_thresh["indicator"] = vs_pop_thresh["State_Code"] +"-"+vs_pop_thresh["County_Name"]
 
 
+# %%
+vs_pop_thresh = vs_pop_thresh.loc[vs_pop_thresh["indicator"].isin(county)]
+
+
+# %%
+len(vs_pop_thresh)
+vs_pop_thresh.State_Code.value_counts()
+
+
+# %%
+vs_pop_thresh.Deaths.isna().sum()
+
+
+
+# %%
+vs_pop_thresh.to_csv(
+    "/Users/Aarushi/Duke/MIDS - Fall 2021/Fall 2021/"
+    "720_IDS_PDS/pds2021-opioids-team-2-ids720/"
+    "20_intermediate_files/mortality_merged_with_pop_thresh.csv",
+    encoding="utf-8",
+    index=False,
+)
+
+# %%
